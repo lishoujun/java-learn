b6d2f8c21e630dc508ee667902fc520608fdb524
lishoujun <lsjun@aliyun.com>
2017-08-19 15:15:01 +0800

    first commit

---
+++ src/main/java/test/test/App.java
@@ -0,0 +1,16 @@
+package test.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	for (int i = 0; true; i++) {
+    		String s = "Hello World!" ;
+		}
+        
+    }
+}
